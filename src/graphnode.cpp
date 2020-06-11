3b2dd16678c6338e61f0c730df77f9a6ffaef654
stickmonster <40922682+stickmonster@users.noreply.github.com>
2020-06-11 14:37:33 +0100

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -37,13 +37,13 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 ////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot = chatbot;
+    _chatBot = std::move(chatbot);
     _chatBot.SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(_chatBot);
+    newNode->MoveChatbotHere(std::move(_chatBot));
    // _chatBot = nullptr; // invalidate pointer at source
 }
 ////
